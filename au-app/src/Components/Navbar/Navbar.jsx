4b786250d5808a8a45c1bd3d0373d3b89747ca72
aycaakin <aycaakin16@gmail.com>
2024-07-02 14:57:41 +0300

    a

--- au-app/src/Components/Navbar/Navbar.jsx
+++ au-app/src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 import logo from '../Assets/logo.png';
-import cart_icon from '../Assets/cart_icon.png';
 import profile_icon from '../Assets/profile_icon.png';
 import { instance } from '../../api';
 
@@ -47,8 +46,7 @@ const Navbar = ({ loggedIn, onLogout, username }) => {
         ) : (
           <Link to='/login'><button>Login</button></Link>
         )}
-        <Link to='/cart'><img src={cart_icon} alt="Cart Icon" /></Link>
-        <div className='nav-cart-count'>0</div>
+       
       </div>
     </div>
   );
